d0a233f24c93e16035facbe71f6831ec09461088
zorbav <sanghunkimv@gmail.com>
2021-07-18 18:22:22 +0900

    Updates

---
+++ precache-manifest.b9cb99ef11df596e22ea88454864ae40.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6fbb4779ba75de5f5d83799e081fe30",
+    "url": "/movie_app_2021/index.html"
+  },
+  {
+    "revision": "1de17e2f91bf059406fa",
+    "url": "/movie_app_2021/static/css/main.7450030f.chunk.css"
+  },
+  {
+    "revision": "5a33be03a4fa0a55c930",
+    "url": "/movie_app_2021/static/js/2.4ad206ef.chunk.js"
+  },
+  {
+    "revision": "1de17e2f91bf059406fa",
+    "url": "/movie_app_2021/static/js/main.7f7b728e.chunk.js"
+  },
+  {
+    "revision": "2e0cce583920c9bddc4a",
+    "url": "/movie_app_2021/static/js/runtime~main.25bd41d2.js"
+  }
+]);
